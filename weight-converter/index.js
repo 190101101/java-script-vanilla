bd3a992138e358473bf252e17b80d7563dc5224c
190101101 <190101101@mail.ru>
2023-11-16 18:23:29 +0400

    weight-converter

--- weight-converter/index.js
+++ weight-converter/index.js
@@ -1,24 +1,27 @@
-const inputEl = document.getElementById("input");
-const errorEl = document.getElementById("error");
-const resultEl = document.getElementById("result");
+const input = document.getElementById('input');
+const error = document.getElementById('error');
+const result = document.getElementById('result');
+
 let errorTime;
 let resultTime;
-function updateResults() {
-  if (inputEl.value <= 0 || isNaN(inputEl.value)) {
-    errorEl.innerText = "Please enter a valid number!";
+
+const updateResult = () => {
+   if(input.value <= 0 || isNaN(input.value)) {
+    error.innerText = 'Please enter a valid number!';
     clearTimeout(errorTime);
     errorTime = setTimeout(() => {
-      errorEl.innerText = "";
-      inputEl.value = "";
-    }, 2000);
-  } else {
-    resultEl.innerText = (+inputEl.value / 2.2).toFixed(2);
+      error.innerText = '';
+      input.value = '';
+    }, 2000);  
+  }
+  else{
+    result.innerText = (Number(input.value / 2.2).toFixed(2));
     clearTimeout(resultTime);
     resultTime = setTimeout(() => {
-      resultEl.innerText = "";
-      inputEl.value = "";
-    }, 10000);
+      result.innerText = '';
+      input.value = '';
+    }, 5000);
   }
 }
 
-inputEl.addEventListener("input", updateResults);
+input.addEventListener('input', updateResult);
